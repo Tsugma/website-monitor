cec4ccd6d41819f46310ac426f1c59a5a7586514
tsugma <tsugma.kumo@gmail.com>
2018-04-16 21:07:12 +0800

    add Date the output

--- check.sh
+++ check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 URL=sme-urls.txt
+DATE=`date +%Y-%m-%d:%H:%M:%S`
 
 while read website; do
     if [ ! -z "${website}" ]; then
@@ -8,10 +9,10 @@ while read website; do
 
         if echo $CURL | grep "200 OK" > /dev/null
         then
-            echo "($website) is up."
+            echo "<$DATE> ($website) is up."
 
         else
-            echo "($website) is down, Please check."
+            echo "<$DATE> ($website) is down, Please check."
         fi
     fi
 done < $URL
